a04981ae8056f68dcc1eacb112e81afc915290ca
Mohammad Reza <themamiza@gmail.com>
2022-03-11 13:22:33 +0330

    Added isfloating (1) to pavucontrol

--- config.h
+++ config.h
@@ -43,6 +43,7 @@ static const Rule rules[] = {
 	/* class      		instance    title       	tags mask     isfloating  isterminal  noswallow  monitor */
 	{ "Galculator", 	NULL,       NULL,       	0,            1,	  0,	      0,         -1 },
 	{ "TelegramDesktop", 	NULL,       NULL,       	0,            1,	  0,	      0,         -1 },
+	{ "Pavucontrol", 	NULL,       NULL,       	0,            1,	  0,	      0,         -1 },
 	{ TERMCLASS,		NULL,       NULL,     		0,            0,	  1,	      0,         -1 },
 	{ "st", 	        NULL,       NULL,       	0,            0,	  1,	      0,         -1 },
 	{ NULL, 		NULL,       "Event Tester",     0,            0,	  0,	      1,         -1 },
