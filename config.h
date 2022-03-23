7f94ec540beb6d813c7b2bda3ef85559baf287ca
Mohammad Reza <themamiza@gmail.com>
2022-03-23 21:22:18 +0430

    Added rule for lxpolkit

--- config.h
+++ config.h
@@ -45,6 +45,7 @@ static const Rule rules[] = {
 	{ "TelegramDesktop", 	NULL,       NULL,       	0,            1,	  0,	      0,         -1 },
 	{ "Pavucontrol", 	NULL,       NULL,       	0,            1,	  0,	      0,         -1 },
 	{ "Blueman-manager", 	NULL,       NULL,       	0,            1,	  0,	      0,         -1 },
+	{ "Lxpolkit",		NULL,       NULL,       	0,            1,	  0,	      0,         -1 },
 	{ TERMCLASS,		NULL,       NULL,     		0,            0,	  1,	      0,         -1 },
 	{ "st", 	        NULL,       NULL,       	0,            0,	  1,	      0,         -1 },
 	{ NULL, 		NULL,       "Event Tester",     0,            0,	  0,	      1,         -1 },
