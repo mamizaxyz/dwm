4d1020b0cbab0cada3ac468bf3c5c2e33f275032
Mohammad Reza <themamiza@gmail.com>
2022-03-26 20:25:40 +0430

    Replaced 'alacritty' with the 'TERMINAL' valuable

--- config.h
+++ config.h
@@ -79,9 +79,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { TERMINAL, "-t", scratchpadname, NULL };
 
 #include <X11/XF86keysym.h>
 
